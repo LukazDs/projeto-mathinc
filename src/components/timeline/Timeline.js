df451bf25a4fcd14b511234ce8c62ea0ee41b32e
LukazDs <lukazds2016@gmail.com>
2022-10-05 22:59:17 -0300

    feat: validate token in posts screen

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,19 +1,40 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../contexts/UserContext";
 import Hearder from "../header/Header";
 import Post from "../post/Post";
 import { Container, PostsContainer } from "./timelineStyle";
 
 function Timeline() {
   const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+  const { token } = useState(UserContext);
+
+  const tokenDefault = token ? token : localStorage.getItem("token");
 
   useEffect(() => {
-    const promise = axios.get(`${process.env.REACT_APP_API_BASE_URL}/posts`);
+    const config = { headers: { Authorization: `${tokenDefault}` } };
 
-    promise.then((res) => {
-      setPosts(res.data);
-    });
-  }, []);
+    if (!tokenDefault) navigate("/");
+
+    const promise = axios.get(
+      `${process.env.REACT_APP_API_BASE_URL}/posts`,
+      config
+    );
+
+    promise
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        alert(
+          "An error occured while trying to fetch the posts, please refresh the page"
+        );
+        localStorage.clear();
+        navigate("/");
+      });
+  });
 
   function makePosts() {
     return posts.map((v, i) => {
